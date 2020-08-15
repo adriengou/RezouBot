4f1fddbdc794c534a07afc190b19b1777239abcd
aquilon74 <aquilon74@gmail.com>
2020-08-16 00:08:47 +0200

    reading token form external file

--- rezou_bot.js
+++ rezou_bot.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,9 +7,31 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    if(msg.author.username === client.user.username){
+        return false;
+    }
+
+    if(msg.channel.name !== 'bot'){
+        msg.delete();
+        msg.reply('This is not the right channel')
+        return false;
+    }
+
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+
+    
 });
 
-client.login('NTA0NTc4NDIwNjQ3MjY0MjY2.W9AzRg.AJzBl3pwBdaf37fVl86DhZz0QSM');
+
+//Reading token.txt to get the token.
+fs.readFile('token.txt', 'utf8', function(err, data){
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    token = data;
+    console.log(`Token: ${token}`)
+    client.login(token);
+});
